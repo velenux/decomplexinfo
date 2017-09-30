ab335f69cdca2e2ff3966872742487029a7f24ce
Gilberto Ficara <g.ficara@stardata.it>
2017-09-30 12:09:47 +0200

    Fix: disabled error output on the docker rm operation since we may not have any decomplexinfo container.

--- docker_build.sh
+++ docker_build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # kill all decomplexinfo containers
-docker ps --all | grep decomplexinfo | awk {'print $1'} | xargs docker rm
+docker ps --all | grep decomplexinfo | awk {'print $1'} | xargs docker rm 2>/dev/null
 
 # remove previous images
 docker rmi decomplexinfo
 
 # rebuild the image
 docker build -t decomplexinfo .
-
--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # kill all decomplexinfo containers
-docker ps --all | grep decomplexinfo | awk {'print $1'} | xargs docker rm
+docker ps --all | grep decomplexinfo | awk {'print $1'} | xargs docker rm 2> /dev/null
 
 # remove previous images
 docker rmi decomplexinfo
