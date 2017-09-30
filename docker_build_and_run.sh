3410092fca8936b69c6c07d07379d4899af974a0
Gilberto Ficara <g.ficara@stardata.it>
2017-09-30 12:08:40 +0200

    Update: update the build script to get rid of old containers if required.

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# kill all decomplexinfo containers
+docker ps --all | grep decomplexinfo | awk {'print $1'} | xargs docker rm
+
+# remove previous images
 docker rmi decomplexinfo
+
+# rebuild the image and run it
 docker build -t decomplexinfo . && ./docker_run.sh
